36da3c58558bcb4fafa855645b4ead21a588211d
Matt <mddever@gmail.com>
2019-07-10 20:13:27 -0600

    created maping return of todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import useState from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([
+      {text: "Learn about React" },
+      { text: "Meet Friend for lunch" },
+      { text: "Build really colol todo app" }
+    ]);
+
+    const Todo = ({ todo }) =><div classname="todo">{todo.text}</div>
+      
+    return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            />
+          ))}
+          </div>
+          </div>
+    );
+  
 }
 
 export default App;
